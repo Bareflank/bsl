4f68a94607e75b8e55cc6707805096484adaa3ca
Rian Quinn <rianquinn@gmail.com>
2020-05-05 05:02:44 -0600

    Move verbosity to the info target
    This patch moves some of the verbosity of the bsl build system
    to the info target so that you can still get the build
    connfiguration, but it is not duplicated with FetchContent

--- cmake/target/info.cmake
+++ cmake/target/info.cmake
@@ -32,27 +32,112 @@ add_custom_command(TARGET info
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Please give us a star on: ${BF_COLOR_WHT}https://github.com/Bareflank "
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --blue    " ------------------------------------------------------ "
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color " "
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Current Build Configuration:"
     VERBATIM
 )
 
+# ------------------------------------------------------------------------------
+# options
+# ------------------------------------------------------------------------------
+
+if(BUILD_EXAMPLES)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BUILD_EXAMPLES                 ${BF_ENABLED}"
+        VERBATIM
+    )
+else()
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BUILD_EXAMPLES                 ${BF_DISABLED}"
+        VERBATIM
+    )
+endif()
+
+if(BUILD_TESTS)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BUILD_TESTS                    ${BF_ENABLED}"
+        VERBATIM
+    )
+else()
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BUILD_TESTS                    ${BF_DISABLED}"
+        VERBATIM
+    )
+endif()
+
+if(ENABLE_CLANG_FORMAT)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ENABLE_CLANG_FORMAT            ${BF_ENABLED} - ${BF_CLANG_FORMAT}"
+        VERBATIM
+    )
+else()
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ENABLE_CLANG_FORMAT            ${BF_DISABLED}"
+        VERBATIM
+    )
+endif()
+
+if(ENABLE_DOXYGEN)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ENABLE_DOXYGEN                 ${BF_ENABLED} - ${BF_DOXYGEN}"
+        VERBATIM
+    )
+else()
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ENABLE_DOXYGEN                 ${BF_DISABLED}"
+        VERBATIM
+    )
+endif()
+
+# ------------------------------------------------------------------------------
+# settings
+# ------------------------------------------------------------------------------
+
 add_custom_command(TARGET info
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Supported CMake Build Types:"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=RELEASE            compile in release mode"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=DEBUG              compile in debug mode"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=CLANG_TIDY         compile with Clang Tidy checks"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=PERFORCE           compile with Perforce checks"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=ASAN               compile with Google ASAN"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=UBSAN              compile with Google UBSAN"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   -DCMAKE_BUILD_TYPE=CODECOV            compile with LLVM coverage"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BSL_DEBUG_LEVEL                ${BF_COLOR_CYN}${BSL_DEBUG_LEVEL}"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   BSL_PAGE_SIZE                  ${BF_COLOR_CYN}${BSL_PAGE_SIZE}"
+    VERBATIM
+)
+
+# ------------------------------------------------------------------------------
+# build type
+# ------------------------------------------------------------------------------
+
+if(CMAKE_BUILD_TYPE STREQUAL CLANG_TIDY)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   CMAKE_BUILD_TYPE               ${BF_COLOR_CYN}${CMAKE_BUILD_TYPE}${BF_COLOR_RST} - ${CMAKE_CXX_CLANG_TIDY}"
+        VERBATIM
+    )
+elseif(CMAKE_BUILD_TYPE STREQUAL CODECOV)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   CMAKE_BUILD_TYPE               ${BF_COLOR_CYN}${CMAKE_BUILD_TYPE}${BF_COLOR_RST} - ${BF_GRCOV}"
+        VERBATIM
+    )
+else()
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   CMAKE_BUILD_TYPE               ${BF_COLOR_CYN}${CMAKE_BUILD_TYPE}"
+        VERBATIM
+    )
+endif()
+
+# ------------------------------------------------------------------------------
+# remaining info
+# ------------------------------------------------------------------------------
+
+add_custom_command(TARGET info
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color " "
-    VERBATIM
-)
-
-add_custom_command(TARGET info
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Supported CMake Build Types:"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=RELEASE      compile in release mode"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=DEBUG        compile in debug mode"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=CLANG_TIDY   compile with Clang Tidy checks"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=PERFORCE     compile with Perforce checks"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=ASAN         compile with Google ASAN"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=UBSAN        compile with Google UBSAN"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "  -DCMAKE_BUILD_TYPE=CODECOV      compile with LLVM coverage"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color " "
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Basic Commands:"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} info                            shows this help info"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND}                                 builds the project"
-    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} clean                           cleans the project"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} info                     shows this help info"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND}                          builds the project"
+    COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} clean                    cleans the project"
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color " "
     COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --green   " Supported Build Targets:"
     VERBATIM
@@ -60,41 +145,29 @@ add_custom_command(TARGET info
 
 if(BUILD_TESTS)
     add_custom_command(TARGET info
-        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} unittest                        run the project's unit tests"
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} unittest                 run the project's unit tests"
         VERBATIM
     )
 endif()
-
-# ------------------------------------------------------------------------------
-# clang format
-# ------------------------------------------------------------------------------
 
 if(ENABLE_CLANG_FORMAT)
     add_custom_command(TARGET info
-        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} format                          formats the source code"
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} format                   formats the source code"
         VERBATIM
     )
 endif()
-
-# ------------------------------------------------------------------------------
-# llvm-cov
-# ------------------------------------------------------------------------------
-
-if(CMAKE_BUILD_TYPE STREQUAL CODECOV)
-    add_custom_command(TARGET info
-        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} codecov-info                   gathers info about unit test coverage"
-        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} codecov-upload                 uploads results of unit test coverage"
-        VERBATIM
-    )
-endif()
-
-# ------------------------------------------------------------------------------
-# doxygen
-# ------------------------------------------------------------------------------
 
 if(ENABLE_DOXYGEN)
     add_custom_command(TARGET info
-        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} doxygen                         generates documentation"
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} doxygen                  generates documentation"
+        VERBATIM
+    )
+endif()
+
+if(CMAKE_BUILD_TYPE STREQUAL CODECOV)
+    add_custom_command(TARGET info
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} codecov-info            gathers info about unit test coverage"
+        COMMAND ${CMAKE_COMMAND} -E cmake_echo_color --yellow  "   ${BUILD_COMMAND} codecov-upload          uploads results of unit test coverage"
         VERBATIM
     )
 endif()
